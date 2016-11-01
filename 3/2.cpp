d07e5a7bd8e5a921de41bf54f4d5816997a31c8b
BZz13 <BZz13@skbkontur.ru>
2016-11-01 15:46:35 +0500

    bzz13: 2 stack

---
+++ 3/2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <ostream>
+#include <utility>
+#include <stack>
+#include <vector>
+
+template<typename T>
+class stack_with_min
+{
+    std::stack<T> m_values;
+    std::stack<T> m_minims;
+public:
+    void push(const T data)
+    {
+        m_values.push(data);
+        m_minims.push((m_minims.empty() || data < m_minims.top() ? data : m_minims.top()));
+    }
+
+    std::pair<T,T> pop()
+    {
+        if (!m_values.empty())
+        {
+            auto result = std::make_pair(m_values.top(), m_minims.top());
+            m_values.pop();
+            m_minims.pop();
+            return result;
+        }
+        throw "stack is empty";
+    }
+
+    operator bool(){
+        return !m_values.empty();
+    }
+};
+
+int main()
+{
+    stack_with_min<int> s;
+    s.push(5);
+    s.push(6);
+    s.push(1);
+    s.push(3);
+
+    while(s)
+    {
+        auto p = s.pop();
+        std::cout << p.first << ": " << p.second <<std::endl;
+    }
+    return 0;
+}
