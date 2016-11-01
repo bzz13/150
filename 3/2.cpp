b48960c5b192c979cef9a152128026a1873d313e
BZz13 <BZz13@skbkontur.ru>
2016-11-01 15:50:57 +0500

    bzz13: 2 stack

--- 3/2.cpp
+++ 3/2.cpp
@@ -33,9 +33,45 @@ public:
     }
 };
 
+
+template<typename T>
+class stack_with_min2
+{
+    std::stack<std::pair<T, T>> m_values;
+public:
+    void push(const T data)
+    {
+        if (m_values.empty())
+        {
+            m_values.push(std::make_pair(data, data));
+        }
+        else
+        {
+            auto top = m_values.top();
+            m_values.push(std::make_pair(data, top.second < data ? top.second : data));
+        }
+
+    }
+
+    std::pair<T,T> pop()
+    {
+        if (!m_values.empty())
+        {
+            auto result = m_values.top();
+            m_values.pop();
+            return result;
+        }
+        throw "stack is empty";
+    }
+
+    operator bool(){
+        return !m_values.empty();
+    }
+};
+
 int main()
 {
-    stack_with_min<int> s;
+    stack_with_min2<int> s;
     s.push(5);
     s.push(6);
     s.push(1);
