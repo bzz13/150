26565e6cc5aa37a6742bd5c8ff87697d0e1a3974
BZz13 <BZz13@skbkontur.ru>
2016-11-03 16:02:55 +0500

    bzz13: simplify

--- 10/quikSort.cpp
+++ 10/quikSort.cpp
@@ -2,7 +2,7 @@
 
 int partition(int* arr, const int left, const int right)
 {
-    int pivot = arr[right - 1];
+    int pivot = arr[right - 1]; // can randomize
     int i = left;
     int j = right - 1;
     while(i < j)
@@ -22,17 +22,17 @@ int partition(int* arr, const int left, const int right)
 }
 void quikSort(int* arr, const int left, const int right)
 {
-    if (left < right)
+    if (left < right - 1)
     {
         int index = partition(arr, left, right);
-        if (left < index && index < right) quikSort(arr, left, index);
-        if (index < right - 1) quikSort(arr, index, right);
+        if (index < right) quikSort(arr, left, index);
+        quikSort(arr, index, right);
     }
 }
 
 int main()
 {
-    int nums[]{3, 4, 6, 2, 5, 8, 9, 0, 1, 7, 4};
+    int nums[]{3, 4, 1, 4, 6, 2, 5, 8, 9, 0, 1, 7, 4};
     quikSort(nums, 0, sizeof(nums)/sizeof(nums[0]));
     for(int i: nums)
     {
