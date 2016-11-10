39e62476a837a09a2faf422be1c065fe767b7913
BZz13 <BZz13@skbkontur.ru>
2016-11-10 13:46:05 +0500

    bzz13: last task from ya

--- task.cpp
+++ task.cpp
@@ -1,4 +1,81 @@
+#include <iostream>
+#include <stack>
+
+//a -> b -> c
+//c -> b -> a
+
+template<typename T>
+class Node
+{
+public:
+    T data;
+    Node<T>* next;
+};
+
+template<typename T>
+Node<T>* reverse(Node<T>* n)
+{
+    if (!n)
+    {
+        return n;
+    }
+    std::stack<Node<T>*> st;
+    auto runner = n;
+    while(runner)
+    {
+        st.push(runner);
+        runner = runner->next;
+    }
+    
+    n = st.top();
+    st.pop();
+    
+    runner = n;
+    while(!st.empty())
+    {
+        runner->next = st.top();
+        runner = runner->next;
+        runner->next = nullptr;
+        st.pop();
+    }
+    return n;
+    
+    /*
+    std::stack<T> st;
+    auto runner = n;
+    while(runner)
+    {
+        st.push(runner->data);
+        runner = runner->next;
+    }
+    
+    runner = n;
+    while(!st.empty())
+    {
+        runner->data = st.top();
+        st.pop();
+        runner = runner->next;
+    }
+    */
+}
+
 int main()
 {
-    return 0;
+    Node<char>* head = new Node<char>{'a', new Node<char>{ 'b', new Node<char>{'c', nullptr}}};
+    
+    auto runner = head;
+    while(runner)
+    {
+        std::cout << runner->data << " -> ";
+        runner = runner->next;
+    }
+    std::cout<<std::endl;
+    head = reverse(head);
+    runner = head;
+    while(runner)
+    {
+        std::cout << runner->data << " -> ";
+        runner = runner->next;
+    }
+    
 }
